f7d67b37e2141de0f2feb48e5ba1b257d5fee91b
programadorisgod <catapiascastro@gmail.com>
2023-12-15 15:23:28 -0500

    -mslot

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Section from './Section.svelte';
+  import svg from './assets/svelte.svg'
+</script>
+
+<main>
+  <img src={svg} alt="svelte_logo">
+
+   <Section title="Menu of the day">
+    <p slot="slot_section">Soup</p>
+    <p>Rice</p>
+    <p>Pizza</p>
+    <p>salad</p>
+    <p>tea</p>
+   </Section>
+
+   <Section title="coffees">
+      <svelte:fragment slot="slot_section">
+        <p>Coffe whit milk</p>
+        <p>Cold coffe</p>
+      </svelte:fragment>
+   </Section>
+</main>
+  
---
+++ src/Section.svelte
@@ -0,0 +1,13 @@
+<script>
+    export let title
+</script>
+
+ <header>
+    <slot name="slot_title"><h1>{title}</h1></slot>
+ </header>
+
+<section>
+    <slot name="slot_section">
+    <p>Mensaje por si no se pasa nada mi padre</p>
+    </slot>
+</section>
